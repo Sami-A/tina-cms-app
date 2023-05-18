947911b024a2bdc90f6feb54582e9c4fa2b7e700
Sami-A <getsamiaiz@gmail.com>
2023-05-18 03:13:30 +0300

    :zap: environment variables are added on tina/config file for cliendId and token

--- tina/config.ts
+++ tina/config.ts
@@ -12,8 +12,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch: "main",
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId: process.env.CLIENT_ID, // process.env.CLIENT_ID,
+  token: process.env.TOKEN, // process.env.TOKEN,
 
   build: {
     outputFolder: "admin",
